2824ddc43e4db79dee7613bd47fd97652b927baa
Igor Kolomiyets <ikolomiyets@gmail.com>
2017-11-18 17:58:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bootstrap starter pack
+
+This project creates a webpack based canvas for the frontend applications using bootstrap
+
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack');
+const path = require('path');
+ 
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],
+        // In case you imported plugins individually, you must also require them here:
+        Util: "exports-loader?Util!bootstrap/js/dist/util",
+        Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      })
+    ],
+    module: {
+      rules: [{
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'style-loader', // inject CSS to page
+            }, 
+            {
+              loader: 'css-loader', // translates CSS into CommonJS modules
+            }, 
+            {
+              loader: 'postcss-loader', // Run post css actions
+              options: {
+                plugins: function () { // post css plugins, can be exported to postcss.config.js
+                  return [
+                    require('precss'),
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            }, 
+            {
+              loader: 'sass-loader' // compiles SASS to CSS
+            }
+          ]
+      }]
+    },
+    devtool: "cheap-module-eval-source-map",
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
